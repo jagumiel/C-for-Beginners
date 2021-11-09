b14b9e2d04ca2d48a452853b94ac17155507a5de
Jose Ángel Gumiel <jagumiel001@gmail.com>
2021-11-09 12:09:28 +0100

    Update helloOmp.c

--- parallelComputing/openMP-HelloWorld/helloOmp.c
+++ parallelComputing/openMP-HelloWorld/helloOmp.c
@@ -10,7 +10,8 @@
 int main(int argc, char* argv[]){
  
     // Beginning of parallel region
-    #pragma omp parallel{
+    #pragma omp parallel
+    {
         printf("Hello World... from thread = %d\n",
                omp_get_thread_num());
     }
